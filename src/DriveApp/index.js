35d19f2376487beed5b8099d11d9856cd7ac6611
Matheus Martins <matheusmr00@gmail.com>
2017-08-07 23:17:28 -0300

    drive app methods

--- src/DriveApp/index.js
+++ src/DriveApp/index.js
@@ -31,32 +31,78 @@ class DriveApp {
 		return new Folder(id);
 	}
 
-	addFile(child: File): Folder {
+	static addFile(child: File): Folder {
 		console.info(child);
 	}
-	// addFolder(Folder child) : Folder
-	// continueFileIterator(String continuationToken) : FileIterator
-	// continueFolderIterator(String continuationToken) : FolderIterator
-	// createFile(BlobSource blob) : File
-	// createFile(String name, String content) : File
-	// createFile(String name, String content, String mimeType) : File
-	// createFolder(String name) : Folder
-	// getFileById(String id) : File
-	// getFiles() : FileIterator
-	// getFilesByName(String name) : FileIterator
-	// getFilesByType(String mimeType) : FileIterator
-	// getFolderById(String id) : Folder
-	// getFolders() : FolderIterator
-	// getFoldersByName(String name) : FolderIterator
-	// getRootFolder() : Folder
-	// getStorageLimit() : Integer
-	// getStorageUsed() : Integer
-	// getTrashedFiles() : FileIterator
-	// getTrashedFolders() : FolderIterator
-	// removeFile(File child) : Folder
-	// removeFolder(Folder child) : Folder
-	// searchFiles(String params) : FileIterator
-	// searchFolders(String params) : FolderIterator
+	static addFolder(child: Folder) : Folder {
+
+	}
+	static continueFileIterator(continuationToken: String) : FileIterator {
+
+	}
+	static continueFolderIterator(continuationToken: String) : FolderIterator {
+
+	}
+	static createFile(blob: BlobSource ) : File {
+
+	}
+	static createFile(name: String, content: String) : File {
+
+	}
+	static createFile(name: String, content: String, mimeType: String) : File {
+
+	}
+	static createFolder(name: String) : Folder {
+
+	}
+	static getFileById(id: String) : File {
+
+	}
+	static getFiles() : FileIterator {
+
+	}
+	static getFilesByName(name: String) : FileIterator {
+
+	}
+	static getFilesByType(mimeType: String) : FileIterator {
+
+	}
+	static getFolderById(id: String) : Folder {
+
+	}
+	static getFolders() : FolderIterator {
+
+	}
+	static getFoldersByName(name: String) : FolderIterator {
+
+	}
+	static getRootFolder() : Folder {
+
+	}
+	static getStorageLimit() : Integer {
+
+	}
+	static getStorageUsed() : Integer {
+
+	}
+	static getTrashedFiles() : FileIterator {
+
+	}
+	static getTrashedFolders() : FolderIterator {
+
+	}
+	static removeFile(child: File) : Folder {
+
+	}
+	static removeFolder( child: Folder) : Folder {
+
+	}
+	static searchFiles( params: String) : FileIterator {
+
+	}
+	static searchFolders( params: String) : FolderIterator {
+
+	}
 }
 
 export default DriveApp;
